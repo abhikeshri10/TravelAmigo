8028d3dc3e2f3fe359f4a1ca8daefa92e3cd29d9
vitthalgoyal <vitthalgoyal17@gmail.com>
2021-04-10 11:18:46 +0530

    login logout in api2

--- Server/hack_server/hack_server/urls.py
+++ Server/hack_server/hack_server/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include("hack_server.api.urls")),
+    path('api2/', include('drf_registration.urls')),
 ]
