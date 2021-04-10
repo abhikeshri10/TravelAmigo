cecb15f26e6583b2284aebb43b727b9834c975e5
vitthalgoyal <vitthalgoyal17@gmail.com>
2021-04-10 12:25:15 +0530

    signup issue solved

--- Server/hack_server/hack_server/settings.py
+++ Server/hack_server/hack_server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
+    'rest_framework.authtoken',
     'phone_field',
     'rest_framework',
     'django_filters',
